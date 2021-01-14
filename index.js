0c6c6b931d1bf350b5a9177330b5d94fe5c0e78a
HyeJeongKo-B2LINK <55426349+HyeJeongKo-B2LINK@users.noreply.github.com>
2021-01-14 14:04:11 +0900

    작업중

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var hamburger = document.querySelector('.hamburger');
 var side = document.querySelector('side')
 var container = document.querySelector('.container');
+var scrollContent = document.querySelector('.scroll-content');
+var header = document.querySelector('header');
+var mainImage = document.querySelector('.main img');
+var cursor = document.querySelector('.dot');
+var title = document.querySelector('h1')
 hamburger.addEventListener('click',function(){
   hamburger.classList.toggle('active');
   if(hamburger.classList.contains('active')){
@@ -13,8 +18,6 @@ hamburger.addEventListener('click',function(){
    document.body.style.overflow = 'auto';
   }
 })
-
-document.addEventListener('scroll',function(e){
-  console.log(window.pageYOffset)
-  
+document.addEventListener('mousemove',function(e){
+  cursor.style.transform = 'translate('+ (e.clientX - 5) +'px,'+ (e.clientY -5) +'px)';
 })
